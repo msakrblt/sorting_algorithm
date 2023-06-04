8ba66b3c5501c5d2c3f8ea5fce4fe42b452d8521
Musa Karabulut <musa.krblt.23@gmail.com>
2023-06-04 18:44:01 +0300

    added Insertion Sort algorithm

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,2 +1,9 @@
 def sort(list):
-    pass
+    for ayrac in range(1,len(list)):
+        if(list[ayrac]<list[ayrac-1]):
+            for eleman in range(ayrac,0,-1):
+                if list[eleman]<list[eleman-1]:
+                    list[eleman], list[eleman - 1] = list[eleman - 1], list[eleman]
+                else:
+                    break
+        print(f'Insertion Sort - {ayrac}. iterasyon: {list}')
